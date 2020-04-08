93bf34d6315d36efe67fbd72fda2e1625fe9bf1e
Micah McCallum <micahmccallum@outlook.com>
2020-04-08 03:05:41 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -1,4 +1,16 @@
 class ChattyStudent < Student
 
+  def raise_hand
+    super
+    10.times{puts "Pick me!"}
+  end
+
+  def hello
+    super
+    puts "How are you doing today? I'm okay, but I'm kind of tired....n, it was so crazy! What, you don't want any spoilers? O
+kay well let me just tell you who died..."
+
+  end
+
 
 end
